b4cdbd4bd9a2c6438c6623b677ac824ec82acc70
ng.phuoc_19 <phuocnn@cyberk.io>
2025-09-28 16:25:12 +0700

    “update”

--- src/app/services/dedicated-lab/components/benefits.tsx
+++ src/app/services/dedicated-lab/components/benefits.tsx
@@ -5,30 +5,30 @@ const Benefits: FC = () => {
   return (
     <section className="py-16 md:py-24 bg-white">
       <div className="container mx-auto px-5">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-20 items-center">
-          <div className="text-left">
-            <h2 className="text-3xl md:text-4xl font-medium text-[#212121] mb-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-12 items-center">
+          <div>
+            <h2 className="text-2xl md:text-4xl text-left md:text-center font-medium text-[#212121] mb-6 md:mb-10">
               Goals & Benefits
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-8">
-              <div className="p-6 bg-[#F6FAFF] rounded-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 md:gap-6">
+              <div className="p-4 md:p-6 bg-[#F6FAFF] rounded-lg">
                 <p className="text-[#4B4B4B] text-base font-light">
                   Ensure stability in developing and operating products with
                   real users.
                 </p>
               </div>
-              <div className="p-6 bg-[#F6FAFF] rounded-lg">
+              <div className="p-4 md:p-6 bg-[#F6FAFF] rounded-lg">
                 <p className="text-[#4B4B4B] text-base font-light">
                   A dedicated team deeply understands your product and company
                   culture.
                 </p>
               </div>
-              <div className="p-6 bg-[#F6FAFF] rounded-lg">
+              <div className="p-4 md:p-6 bg-[#F6FAFF] rounded-lg">
                 <p className="text-[#4B4B4B] text-base font-light">
                   Flexibly scale the team up or down as needed.
                 </p>
               </div>
-              <div className="p-6 bg-[#F6FAFF] rounded-lg">
+              <div className="p-4 md:p-6 bg-[#F6FAFF] rounded-lg">
                 <p className="text-[#4B4B4B] text-base font-light">
                   Optimize costs, reduce risks in staffing and technology.
                 </p>
